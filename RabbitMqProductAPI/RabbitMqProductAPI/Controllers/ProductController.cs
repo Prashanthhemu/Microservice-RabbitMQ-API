6fd56ae6b760b0810e861de61473d41d18da6839
Prashanth Kumar Machani <prashanthhemukumar@gmail.com>
2024-04-22 10:50:48 -0500

    Initial commit
    This project leveraged microservice architecture within the .NET Core framework to develop APIs. It utilized RabbitMQ as a messaging broker, with publishers responsible for pushing messages into RabbitMQ, and subscribers tasked with consuming those messages from the queue.

---
+++ RabbitMqProductAPI/RabbitMqProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMqProductAPI.Interfaces;
+using RabbitMqProductAPI.Models;
+using RabbitMqProductAPI.RabbitMq;
+
+namespace RabbitMqProductAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+        private readonly IRabbitMqProducer _rabitMQProducer;
+        public ProductController(IProductService _productService, IRabbitMqProducer rabitMQProducer)
+        {
+            productService = _productService;
+            _rabitMQProducer = rabitMQProducer;
+        }
+        [HttpGet("productlist")]
+        public IEnumerable<Product> ProductList()
+        {
+            var productList = productService.GetProductList();
+            return productList;
+        }
+        [HttpGet("getproductbyid")]
+        public Product GetProductById(int Id)
+        {
+            return productService.GetProductById(Id);
+        }
+        [HttpPost("addproduct")]
+        public Product AddProduct([FromBody] Product product)
+        {
+            var productData = productService.AddProduct(product);
+            //send the inserted product data to the queue and consumer will listening this data from queue
+            _rabitMQProducer.SendProductMessage(product);
+            return productData;
+        }
+        [HttpPut("updateproduct")]
+        public Product UpdateProduct(Product product)
+        {
+            return productService.UpdateProduct(product);
+        }
+        [HttpDelete("deleteproduct")]
+        public bool DeleteProduct(int Id)
+        {
+            return productService.DeleteProduct(Id);
+        }
+    }
+}
